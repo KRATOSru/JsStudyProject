a286453f3de695e92ae11b3838b90f03f79e79e9
v-vlkart <vovan198080@inbox.ru>
2019-05-28 21:34:40 +0300

    Added js logics

--- script.js
+++ script.js
@@ -10,12 +10,27 @@ let appData = {
     savings : false,
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется?", ''),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt("Во сколько обойдется?", '');
+for (let i=0; i<2; i++ ){
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt ("Во сколько обойдется?", "");
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 =a4;
+    //прописываем условия ввода пользователю
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null
+        && a != "" && b != "" && a.length < 50) {
 
-alert(appData.budget / 30);
+        console.log("done");
+
+        appData.expenses[a] = b;
+    }else {
+        console.log("not correctly input")
+        i--;
+    }
+};
+
+
+
+
+appData.moneyPerDay = appData.budget/30;
+
+
+alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
