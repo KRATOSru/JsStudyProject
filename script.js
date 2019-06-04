227c9e8e99b7377069defbeba4ac741a939a8459
v-vlkart <vovan198080@inbox.ru>
2019-06-04 20:36:31 +0300

    object created

--- script.js
+++ script.js
@@ -23,70 +23,62 @@ let appData = {
     timeData: time,
     income: [],
     savings: true,
+    chooseExpense: function () { // фун-я выбора обязательных расходов
+        for (let i = 0; i < 2; i++) {
+            let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+                b = prompt ("Во сколько обойдется?", "");
+
+            //прописываем условия ввода пользователю
+            if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+                appData.expenses[a] = b;
+            } else {
+                i--;
+            }
+        };
+    },
+    detectDayBudget : function () {   //Расчет дневного бюджета
+        appData.moneyPerDay = (appData.budget/30).toFixed();//округляем до целых
+        alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+    },
+    detectLevel: function () {     //Расчет уровня достатка
+        if (appData.moneyPerDay < 100){
+            console.log("It's a minimal profit");
+        }else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000){
+            console.log("It's a normal profit");
+        }else if (appData.moneyPerDay > 1000){
+            console.log("It's a hight profit");
+        }else {
+            console.log("It's finance error");
+        }
+    },
+    checkSavings: function() {  //фун-я, которая рассчитывает накопления с депозита если он есть
+        /* проверяем условие есть ли сбережения и узнаем сумму накоплений и под какой%
+         после получения этих данных рассчитываем сколько человек сможет заработать*/
+        if (appData.savings == true){
+            let save = +prompt("Какая сумма сбережений?"),// + для того чтобы получить цифр знач-е
+                percent = +prompt("Какой % ?");
+
+            appData.monthProfit = save/100/12*percent;
+            alert("Доход за месяц с вашего депозита : " + appData.monthProfit);
+        }
+   },
+    chooseOptExpenses: function () {   //Фун-я для определения необязательных расходов
+        for (let i=1; i<=3; i++) {
+            let questionOptExpenses = prompt("Статья необязательных расходов ?");
+            appData.optionalExpenses[i] = questionOptExpenses;//ответ в объект записываем в формате номер[i] - Ответ
+            console.log(appData.optionalExpenses);
+        }
+    },
+    chooseIncome: function(){   // фун-я дополнительного дохода
+
+        //спрашиваем у пользователя, что принесет дополнительный доход
+        let items = prompt('Что принесет доп profit? (ПЕречислите через запятую)', "");
+              //строку данные переводим в массив  команда split
+        appData.income = items.split(', ');
+        //добавляем в конец массива с помощью push
+        appData.income.push(prompt('Может что-то еще добавить?'));
+        //отсортируем по алфавиту
+        appData.income.sort();
+    }
 };
 
-
-// фун-я выбора обязательных расходов
-function chooseExpense(){
-    for (let i = 0; i < 2; i++) {
-        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-            b = prompt ("Во сколько обойдется?", "");
-
-        //прописываем условия ввода пользователю
-        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-            appData.expenses[a] = b;
-        } else {
-            i--;
-        }
-    };
-}
-chooseExpense();
-
-
-     //Расчет дневного бюджета
-function detectDayBudget(){
-    appData.moneyPerDay = (appData.budget/30).toFixed();//округляем до целых
-    alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
-}
-detectDayBudget();
-
-
-        //Расчет уровня достатка
-function detectLevel() {
-    if (appData.moneyPerDay < 100){
-        console.log("It's a minimal profit");
-    }else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000){
-        console.log("It's a normal profit");
-    }else if (appData.moneyPerDay > 1000){
-        console.log("It's a hight profit");
-    }else {
-        console.log("It's finance error");
-   }
-}
-detectLevel();
-
-
-//фун-я, которая рассчитывает накопления с депозита если он есть
-function checkSavings() {
-   /* проверяем условие есть ли сбережения и узнаем сумму накоплений и под какой%
-     после получения этих данных рассчитываем сколько человек сможет заработать*/
-   if (appData.savings == true){
-       let save = +prompt("Какая сумма сбережений?"),// + для того чтобы получить цифр знач-е
-           percent = +prompt("Какой % ?");
-
-       appData.monthProfit = save/100/12*percent;
-       alert("Доход за месяц с вашего депозита : " + appData.monthProfit);
-    }
-}
-checkSavings();
-
-
-       //Фун-я для определения необязательных расходов
-function chooseOptExpenses() {
-    for (let i=1; i<=3; i++) {
-        let questionOptExpenses = prompt("Статья необязательных расходов ?");
-        appData.optionalExpenses[i] = questionOptExpenses;//ответ в объект записываем в формате номер[i] - Ответ
-        console.log(appData.optionalExpenses);
-    }
-}
-chooseOptExpenses();
