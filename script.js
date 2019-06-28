a8869fbd8a3bd48e19319366efb5940ed1036429
v-vlkart <vovan198080@inbox.ru>
2019-06-28 20:43:10 +0300

    added events for buttons

--- script.js
+++ script.js
@@ -36,21 +36,51 @@ let startBtn = document.getElementById("start"),//кнопка начать ра
     monthValue = document.querySelector('.month-value'),
     dayValue = document.querySelector('.day-value');
 
-
-
 let money, time;
 
-function start() {
-    money = +prompt("Ваш бюджет на месяц?", '');
+startBtn.addEventListener('click', function () {
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    money = +prompt("Ваш бюджет на месяц?", '');
 
     //isNaN возвращает tru если не цифры
     while (isNaN(money) || money == "" || money == null) {
         money = +prompt("Ваш бюджет на месяц?", '');
     }
-}
-start();
+    appData.budget = money;//зафиксировали бюджет, который указал пол-ль в наш обЪекте
+    appData.timeData = time;//тоже самое со временем
+    budgetValue.textContent = money.toFixed();//полученные данные записываем в строку Доход
+    //если есть input в html коде, то работаем с value а не с textContent
+    yearValue.value = new Date(Date.parse(time)).getFullYear();
+    monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
+    dayValue.value = new Date(Date.parse(time)).getDate();
+});
 
+expensesBtn.addEventListener('click', function () {
+    //получаем сумму всех ценников
+    let sum = 0;
+
+    for (let i = 0; i < expensesItem.length; i++) {
+        let a = expensesItem[i].value,
+            b = expensesItem[++i].value;
+
+        //прописываем условия ввода пользователю
+        if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+            appData.expenses[a] = b;
+            sum += +b;
+        } else {
+            i--;
+        }
+    }
+    expensesValue.textContent = sum;
+});
+
+optionalExpensesBtn.addEventListener('click', function () {
+    for (let i = 0; i <= optionalExpensesItem.length; i++) {
+        let opt = optionalExpensesItem[i].value;
+        appData.optionalExpenses[i] = opt;//все что записали в обЪект
+        optionalExpensesValue.textContent += appData.optionalExpenses[i] + ' ';//перейдет и на страницу
+    }
+});
 
 let appData;
 appData = {
@@ -60,20 +90,9 @@ appData = {
     timeData: time,
     income: [],
     savings: true,
-    chooseExpense: function () { // фун-я выбора обязательных расходов
-        for (let i = 0; i < 2; i++) {
-            let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-                b = prompt("Во сколько обойдется?", "");
-
-            //прописываем условия ввода пользователю
-            if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
-                appData.expenses[a] = b;
-            } else {
-                i--;
-            }
-        }
-        ;
-    },
+    // chooseExpense: function () { // фун-я выбора обязательных расходов
+    //
+    // },
     detectDayBudget: function () {   //Расчет дневного бюджета
         appData.moneyPerDay = (appData.budget / 30).toFixed();//округляем до целых
         alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
@@ -96,17 +115,13 @@ appData = {
             let save = +prompt("Какая сумма сбережений?"),// + для того чтобы получить цифр знач-е
                 percent = +prompt("Какой % ?");
 
-            appData.monthProfit = save / 100 / 12 * percent;
+            appData.monthProfit = save / 100 / 12 * percent;// заменена appData.monthIncome
             alert("Доход за месяц с вашего депозита : " + appData.monthProfit);
         }
     },
-    chooseOptExpenses: function () {   //Фун-я для определения необязательных расходов
-        for (let i = 1; i <= 3; i++) {
-            let questionOptExpenses = prompt("Статья необязательных расходов ?");
-            appData.optionalExpenses[i] = questionOptExpenses;//ответ в объект записываем в формате номер[i] - Ответ
-            console.log(appData.optionalExpenses);
-        }
-    },
+    // chooseOptExpenses: function () {   //Фун-я для определения необязательных расходов
+    //
+    // },
     chooseIncome: function () {   // фун-я дополнительного дохода
 
         //спрашиваем у пользователя, что принесет дополнительный доход
